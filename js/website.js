0973e8f689c30487a7e7b785d45a7bdf86de8f39
Samuel pires <samuel.pires.pro@gmail.com>
2018-10-11 17:59:32 +0200

    firs structure for the navbar with js

---
+++ js/website.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".navbar-wagon");
+console.log(navbar);
+navbar.classList.add("transparent-bckgrd");
+const body = document.querySelector("body");
+console.log(body);
+console.log(window.pageYOffset);
+// document.addEventListener("scroll", (event) => {
+//   navbar.classList.add("transparent-bckgrd");
+// });
